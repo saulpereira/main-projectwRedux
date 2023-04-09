22d679bc8e19da779e36fc08be961d80d3a6b682
Saul Pereira <saul_pereira@hotmail.com>
2023-04-09 12:33:08 -0600

    Dispatching Actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -6,5 +6,6 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
-    payload: Ingredient;
+
+    constructor( public payload: Ingredient) {}
 }
