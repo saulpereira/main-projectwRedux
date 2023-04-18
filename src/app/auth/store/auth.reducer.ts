40489d4738221c5eabae9532af03c677de09b34d
Saul Pereira <saul_pereira@hotmail.com>
2023-04-17 18:13:46 -0600

    Auth finished for Now

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions  from './auht.actions';
 
 export interface State {
     user: User
@@ -8,6 +9,31 @@ const initialState: State = {
     user: null
 }
 
-export function authReducer(state = initialState, action) {
+export function authReducer(
+    state = initialState, 
+    action: AuthActions.AuthActions
+    ) {
+
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            const user = new User(
+                action.payload.email, 
+                action.payload.userId, 
+                action.payload.token, 
+                action.payload.expirationDate
+                );
+                return {
+                    ...state,
+                    user: user
+                }
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            }
+        default:
+            return state;
+    }
+
     return state;
 }
